19620a1552b22b4c22b21ad0335aed6e277c3ee4
Jesus <U21309992@utp.edu.pe>
2026-05-31 15:35:45 -0500

    Proyecto Spring Boot inicial

---
+++ src/main/java/com/servitareas/servitareas/ServitareasApplication.java
@@ -0,0 +1,13 @@
+package com.servitareas.servitareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServitareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServitareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servitareas/servitareas/ServitareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servitareas.servitareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServitareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
